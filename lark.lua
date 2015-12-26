e95b559118c9c1b53fa2a9485b2439782ba5880e
Bryan Matsuo <bryan.matsuo@gmail.com>
2015-12-25 21:12:20 -0800

    add 'generate' and 'install' targets in lark.lua

--- lark.lua
+++ lark.lua
@@ -1,4 +1,14 @@
-lark.task{'build', function ()
+lark.task{'generate', function ()
     lark.exec{'go', 'generate', './cmd/...'}
+end}
+
+
+lark.task{'build', function ()
+    lark.run_task{'generate'}
     lark.exec{'go', 'build', './cmd/...'}
+end}
+
+lark.task{'install', function ()
+    lark.run_task{'generate'}
+    lark.exec{'go', 'install', './cmd/...'}
 end}
