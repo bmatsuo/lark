b1269b3e54872f9c9e233cf637ded7cf16d17fbd
Bryan Matsuo <bryan.matsuo@gmail.com>
2015-12-27 23:23:24 -0800

    rename the "generate" task to "gen"

--- lark.lua
+++ lark.lua
@@ -1,21 +1,22 @@
 lark.default_task = 'build'
 
-lark.task{'generate', function ()
+lark.task{'gen', function ()
     lark.exec{'go', 'generate', './cmd/...'}
 end}
 
 lark.task{'build', function ()
-    lark.run{'generate'}
+    lark.run{'gen'}
     lark.exec{'go', 'build', './cmd/...'}
 end}
 
 lark.task{'install', function ()
-    lark.run{'generate'}
+    lark.run{'gen'}
     lark.exec{'go', 'install', './cmd/...'}
 end}
 
 -- BUG: We don't want to test the vendored packages.  But we want to run the
 -- tests for everything else.
 lark.task{'test', function()
+    lark.run{'gen'}
     lark.exec{'go', 'test', '-cover', './cmd/...'}
 end}
