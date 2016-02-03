307eef5e118da7529ca91f736dd60debc1380b40
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-02-03 04:36:58 -0800

    dev: fix ldflags by making it a global variable

--- lark.lua
+++ lark.lua
@@ -6,7 +6,7 @@ local import = 'github.com/bmatsuo/lark'
 local _ldflags = {
     string.format('-X %s/larkmeta.Version=%s', import, version.get()),
 }
-local ldflags = table.concat(_ldflags, ' ')
+ldflags = table.concat(_ldflags, ' ')
 
 go.default_sources = {
     './cmd/...',
