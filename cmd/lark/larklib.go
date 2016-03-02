48ed262ed47f69e811a67966d586cb1d2154d6b4
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-03-02 01:45:24 -0800

    fix list of tested packages

--- cmd/lark/larklib.go
+++ cmd/lark/larklib.go
@@ -160,14 +160,17 @@ lark.exec = function (args)
     args._str = lark.shell_quote(args)
     local result = core.exec(args)
 
-    if args.ignore and result.error then
-        if lark.verbose then 
-            local msg = string.format('%s (ignored)', result.error)
+    local output = result.output
+    local err = result.err
+    if args.ignore and err then
+        if lark.verbose then
+            local msg = string.format('%s (ignored)', err)
             lark.log{msg, color='yellow'}
         end
     elseif result.error then
         error(result.error)
     end
+    return output, err
 end
 
 lark.start = function(args)
