65a68ddcd0913940fefd52b7bbadd045841a244a
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-03-04 23:12:45 -0800

    dev: migrate lark tasks to the new task module API
    There are no more warnings when running project tasks.

--- lark.lua
+++ lark.lua
@@ -1,3 +1,6 @@
+-- the following line migrates from the v0.4.0 task API to v0.5.0 API
+lark.task = require('lark.task')
+
 --local doc = require('doc')
 local go = require('go')
 local version = require('version')
@@ -15,8 +18,8 @@ local novendor =
 string.gsub(novendor, '(%S+)', function(p) table.insert(sources, p) end)
 go.default_sources = sources
 
-lark.task('all', function()
+all = lark.task .. function()
     lark.run('gen')
     lark.run('test')
     lark.run('build')
-end)
+end
