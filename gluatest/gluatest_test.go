0a5581f3d7afee2a5c9a9a96f5aad0c217e9bfab
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-03-06 01:47:56 -0800

    gluatest: add tests for core functionality
    testing execution order, successful tests runs, failed test runs, setup
    and teardown.

---
+++ gluatest/gluatest_test.go
@@ -0,0 +1,190 @@
+package gluatest
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/bmatsuo/lark/gluamodule"
+	"github.com/yuin/gopher-lua"
+)
+
+var testFile = &File{
+	Module: gluamodule.New("test.module", basicTestLoader),
+	Path:   "gluatest_test.lua",
+}
+
+func TestFile(t *testing.T) {
+	l := testFile.Load(t)
+	defer l.Close()
+
+	l.Push(l.GetGlobal("helper"))
+	err := l.PCall(0, 1, nil)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if l.Get(1).String() != "HELP!" {
+		t.Errorf("return value: %q (!= %q)", l.Get(1), "HELP!")
+	}
+}
+
+func TestFile_sorted(t *testing.T) {
+	funcs := testFile.getTestFuncs(t)
+	sorted := make([]string, len(funcs))
+	copy(sorted, funcs)
+	for i := range sorted {
+		if sorted[i] != funcs[i] {
+			t.Errorf("function %d: %q (!= %q)", i, funcs[i], sorted[i])
+		}
+	}
+}
+
+func TestFile_runTest(t *testing.T) {
+	state := map[string]interface{}{}
+	testFile := &File{
+		Module: gluamodule.New("test.module", statefulLoader(state)),
+		Path:   "gluatest_test.lua",
+	}
+	testfn := "test_a"
+	testFile.runTest(t, testfn, getGlobalFunction(testfn))
+	if !reflect.DeepEqual(state["__test_setup"], true) {
+		t.Errorf("%s: %v (!= %v)", "__test_setup", state["__test_setup"], true)
+	}
+	if !reflect.DeepEqual(state["__test_teardown"], true) {
+		t.Errorf("%s: %v (!= %v)", "__test_teardown", state["__test_teardown"], true)
+	}
+	if !reflect.DeepEqual(state["test_a"], true) {
+		t.Errorf("%s: %v (!= %v)", "test_a", state["test_a"], true)
+	}
+	if !reflect.DeepEqual(state["test_b"], nil) {
+		t.Errorf("%s: %v (!= %v)", "test_b", state["test_b"], true)
+	}
+}
+
+func TestFile_runTest_failure(t *testing.T) {
+	state := map[string]interface{}{}
+	testFile := &File{
+		Module: gluamodule.New("test.module", statefulLoader(state)),
+		Path:   "gluatest_test.lua",
+	}
+	var testFailed bool
+	testfn := "test_fail"
+	recorder := &TBFailRecorder{
+		FailFunc: func() { testFailed = true },
+		TB:       t,
+	}
+	testFile.runTest(recorder, testfn, getGlobalFunction(testfn))
+	if !testFailed {
+		t.Errorf("test did not fail")
+	}
+	if !reflect.DeepEqual(state["test_a"], nil) {
+		t.Errorf("%s: %v (!= %v)", "test_a", state["test_a"], nil)
+	}
+	if !reflect.DeepEqual(state["test_fail"], true) {
+		t.Errorf("%s: %v (!= %v)", "test_fail", state["test_fail"], true)
+	}
+	if !reflect.DeepEqual(state["__test_setup"], true) {
+		t.Errorf("%s: %v (!= %v)", "__test_setup", state["__test_setup"], true)
+	}
+	if !reflect.DeepEqual(state["__test_teardown"], true) {
+		t.Errorf("%s: %v (!= %v)", "__test_teardown", state["__test_teardown"], true)
+	}
+}
+
+type TBFailRecorder struct {
+	ReallyFail bool
+	FailFunc   func()
+	testing.TB
+}
+
+func (t *TBFailRecorder) Fail() {
+	t.FailFunc()
+	if t.ReallyFail {
+		t.TB.Fail()
+	}
+}
+
+func (t *TBFailRecorder) FailNow() {
+	t.Fail()
+	runtime.Goexit()
+}
+
+func (t *TBFailRecorder) Fatal(v ...interface{}) {
+	args := []string{"(witheld fatal) "}
+	for _, v := range v {
+		args = append(args, fmt.Sprint(v))
+	}
+	t.Log(strings.Join(args, ""))
+	t.FailNow()
+}
+
+func (t *TBFailRecorder) Fatalf(format string, v ...interface{}) {
+	args := []string{"(witheld fatal) "}
+	args = append(args, fmt.Sprintf(format, v...))
+	t.Log(strings.Join(args, ""))
+	t.FailNow()
+}
+
+func (t *TBFailRecorder) Error(v ...interface{}) {
+	args := []string{"(witheld error) "}
+	for _, v := range v {
+		args = append(args, fmt.Sprint(v))
+	}
+	t.Log(strings.Join(args, ""))
+	t.Fail()
+}
+
+func (t *TBFailRecorder) Errorf(format string, v ...interface{}) {
+	args := []string{"(witheld error) "}
+	args = append(args, fmt.Sprintf(format, v...))
+	t.Log(strings.Join(args, ""))
+	t.Fail()
+}
+
+func statefulLoader(state map[string]interface{}) lua.LGFunction {
+	return func(l *lua.LState) int {
+		mod := l.NewTable()
+
+		l.SetField(mod, "set_value", l.NewFunction(func(l *lua.LState) int {
+			name := l.CheckString(1)
+			val := l.CheckAny(2)
+			l.SetTop(0)
+			if val == lua.LNil {
+				state[name] = nil
+			} else {
+				switch val := val.(type) {
+				case lua.LString:
+					state[name] = string(val)
+				case lua.LNumber:
+					state[name] = float64(val)
+				case lua.LBool:
+					state[name] = bool(val)
+				default:
+					l.RaiseError("argument #2 is not a string, number, or boolean: %s", val.Type())
+				}
+			}
+			return 0
+		}))
+
+		l.Push(mod)
+		return 1
+	}
+}
+
+func basicTestLoader(l *lua.LState) int {
+	mod := l.NewTable()
+
+	double := l.NewClosure(func(l *lua.LState) int {
+		x := l.CheckNumber(1)
+		l.Push(lua.LNumber(2 * x))
+		return 1
+	}, mod)
+	l.SetField(mod, "double", double)
+
+	l.Push(mod)
+	return 1
+}
---
+++ gluatest/gluatest_test.lua
@@ -0,0 +1,31 @@
+module = require('test.module')
+
+function helper()
+    return 'HELP!'
+end
+
+function __test_setup()
+	module.set_value('__test_setup', true)
+end
+
+function __test_teardown()
+	module.set_value('__test_teardown', true)
+end
+
+function test_a()
+	module.set_value('test_a', true)
+end
+
+function test_b()
+	module.set_value('test_b', true)
+end
+
+function test_fail()
+	module.set_value('test_fail', true)
+	assert(false)
+end
+
+function test_ok()
+	module.set_value('test_ok', true)
+	assert(true)
+end
