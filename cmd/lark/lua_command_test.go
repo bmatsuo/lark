3e9ab845a7b659aaea008c59f13afde0de2d418c
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-03-06 21:00:27 -0800

    lark: ensure that all expected modules appear in help text

--- cmd/lark/lua_command_test.go
+++ cmd/lark/lua_command_test.go
@@ -2,14 +2,47 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 	"testing"
 
+	"github.com/bmatsuo/lark/lib"
 	"github.com/bmatsuo/lark/lib/lark/core"
 	"github.com/codegangsta/cli"
 )
+
+func TestREPLHelp(t *testing.T) {
+	help := REPLHelp()
+
+	marker := "\nBuiltin Modules\n\n"
+	index := strings.Index(help, marker)
+	if index < 0 {
+		t.Fatalf("cannot find module list")
+	}
+
+	modules := help[index+len(marker):]
+mloop:
+	for i, m := range lib.Modules {
+		name := m.Name()
+		for _, intern := range lib.InternalModules {
+			if intern.Name() == name {
+				continue mloop
+			}
+		}
+		patt := fmt.Sprintf(`\s*%s\s`, name)
+		re, err := regexp.Compile(patt)
+		if err != nil {
+			t.Errorf("pattern %d: %v", i, err)
+			continue
+		}
+		if !re.MatchString(modules) {
+			t.Errorf("couldn't find module: %q\n%s", name, modules)
+		}
+	}
+}
 
 func TestLua(t *testing.T) {
 	rec := testLogging(t, true)
