1c52e383904237715dd407c68f0abe1f93d17f03
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-03-06 23:39:17 -0800

    project: test SetProjectPath (unix)

--- project/project_unix_test.go
+++ project/project_unix_test.go
@@ -1,11 +1,38 @@
 package project
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/yuin/gopher-lua"
+)
 
 func TestPackagePath(t *testing.T) {
 	path := PackagePath(".")
 	expect := "lark_modules/?.lua;lark_modules/?/init.lua"
 	if path != expect {
+		t.Errorf("package path: %q (!= %q)", path, expect)
+	}
+}
+
+func TestSetPackagePath(t *testing.T) {
+	dir := "x"
+	expect := "x/lark_modules/?.lua;x/lark_modules/?/init.lua"
+
+	l := lua.NewState()
+	defer l.Close()
+
+	err := SetPackagePath(l, dir)
+	if err != nil {
+		t.Error(err)
+	}
+
+	lvpath := l.GetField(l.GetGlobal("package"), "path")
+	path, ok := lvpath.(lua.LString)
+	if !ok {
+		t.Errorf("package.path is not a string: %s", lvpath.Type())
+	}
+
+	if string(path) != expect {
 		t.Errorf("package path: %q (!= %q)", path, expect)
 	}
 }
